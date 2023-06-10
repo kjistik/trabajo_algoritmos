8b55a150bb8021f12151d9c255941e64cb2f897b
Juan Pablo Mersuglia <60672565+jpmersuglia@users.noreply.github.com>
2023-06-10 04:27:10 -0300

    Update App.java
    Actualizando el menú.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import utils.PersistentMenu;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        PersistentMenu.displaySubMenu();
     }
 }
